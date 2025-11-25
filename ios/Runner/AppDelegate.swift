a892bcdc09ea312a19ac8e30ae6c03c5cd988d01
Michele Lorenzoni <michele.lorenzoni@mailfence.com>
2025-11-25 06:22:44 +0100

    chore(flutter_local_notifications): Add dependency

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,25 @@
 import Flutter
 import UIKit
 
+//Manually added packages
+import flutter_local_notifications
+
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FlutterLocalNotificationsPlugin.setPluginRegistranCallback {
+      (registry) in GeneratedPluginRegistrant.register(with: registry)
+    }
+
     GeneratedPluginRegistrant.register(with: self)
+    
+    if if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    } 
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
